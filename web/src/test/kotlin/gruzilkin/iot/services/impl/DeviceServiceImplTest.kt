4843edda62b30fc4f587f9d464dc777a55999f0f
gruzilkin <gruzmob@gmail.com>
2024-07-27 15:59:58 +0900

    tests

--- web/src/test/kotlin/gruzilkin/iot/services/impl/DeviceServiceImplTest.kt
+++ web/src/test/kotlin/gruzilkin/iot/services/impl/DeviceServiceImplTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 import java.security.Principal
 import kotlin.test.*
 
@@ -14,6 +15,7 @@ class DeviceServiceImplTest {
     lateinit var deviceService: DeviceService
 
     @Test
+    @DirtiesContext
     fun `find by id should not see other user devices`() {
         val user1 = Principal { "1" }
         val user2 = Principal { "2" }
@@ -23,11 +25,33 @@ class DeviceServiceImplTest {
     }
 
     @Test
+    @DirtiesContext
     fun `find all should not see other user devices`() {
         val user1 = Principal { "1" }
         val user2 = Principal { "2" }
         var device = deviceService.save(user1, "device_1")
         var readDevices = deviceService.findAll(user2)
         assertTrue { readDevices.isEmpty() }
+    }
+
+    @Test
+    @DirtiesContext
+    fun `delete by id should not see other user devices`() {
+        val user1 = Principal { "1" }
+        val user2 = Principal { "2" }
+        val device = deviceService.save(user1, "device_1")
+        deviceService.deleteById(user2, device.id!!)
+        val readDevice = deviceService.findById(user1, device.id!!)
+        assertEquals(device, readDevice)
+    }
+
+    @Test
+    @DirtiesContext
+    fun `delete by id green path`() {
+        val user1 = Principal { "1" }
+        val device = deviceService.save(user1, "device_1")
+        deviceService.deleteById(user1, device.id!!)
+        val readDevice = deviceService.findById(user1, device.id!!)
+        assertNull(readDevice)
     }
 }
