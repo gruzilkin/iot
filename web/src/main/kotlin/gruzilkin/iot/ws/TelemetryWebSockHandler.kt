96574c1031cfc16114a9e3d7227f63f26905292e
gruzilkin <gruzmob@gmail.com>
2024-07-28 15:51:59 +0900

    add a websocket endpoint with authentication

---
+++ web/src/main/kotlin/gruzilkin/iot/ws/TelemetryWebSockHandler.kt
@@ -0,0 +1,16 @@
+package gruzilkin.iot.ws
+
+import gruzilkin.iot.repositories.TokenRepository
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.handler.TextWebSocketHandler
+
+@Component
+class TelemetryWebSockHandler(val tokenRepository: TokenRepository) : TextWebSocketHandler() {
+    override fun handleTextMessage(webSocketSession: WebSocketSession, message: TextMessage) {
+        val deviceId = webSocketSession.attributes["deviceId"] as Long
+        println("[$deviceId]Received message: $message")
+        webSocketSession.sendMessage(TextMessage("reply to device $deviceId with $message"))
+    }
+}
