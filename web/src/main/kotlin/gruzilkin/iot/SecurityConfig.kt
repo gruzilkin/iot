0821f3a831f59979f582d0587c3a0c82398666fa
gruzilkin <gruzmob@gmail.com>
2024-11-02 15:36:17 +0900

    more fixes for web socket reconnect

--- web/src/main/kotlin/gruzilkin/iot/SecurityConfig.kt
+++ web/src/main/kotlin/gruzilkin/iot/SecurityConfig.kt
@@ -2,10 +2,14 @@ package gruzilkin.iot
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.OrRequestMatcher
 
 @EnableWebSecurity
 @Configuration
@@ -18,6 +22,16 @@ class SecurityConfig {
                 authorize(anyRequest, authenticated)
             }
             oauth2Login {}
+
+            exceptionHandling {
+                defaultAuthenticationEntryPointFor(
+                    HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                    OrRequestMatcher(
+                        AntPathRequestMatcher("/charts/*/partial"),
+                        AntPathRequestMatcher("/charts/*/realtime")
+                    )
+                )
+            }
         }
         return http.build()
     }
